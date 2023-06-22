eaee60fdd6848e95b0e91ffc74c824185719ea28
Subha-CJPR <56904625+Subha-CJPR@users.noreply.github.com>
2023-06-22 22:10:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function getBrowserInfo() {
+      var browserInfoElement = document.getElementById('browser-info');
+      var browserName = navigator.appName;
+      var version = navigator.appVersion;
+
+      browserInfoElement.textContent = 'You are using ' + browserName + ' version ' + version;
+    }
+
+    // Call the getBrowserInfo function when the page loads
+    window.addEventListener('load', getBrowserInfo);
